04064a2676dce7c3ded7d848c4fdbf909cd658c1
Raghav Narula <git@raghavnarula.co.uk>
2019-12-02 08:25:39 +0000

    Add day 2 part 1

--- Day1/Day1Test/UnitTest1.cs
+++ Day1/Day1Test/UnitTest1.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.Linq;
 using Day1;
+using Day2;
 using NUnit.Framework;
 
 namespace Day1Test
 {
-    public class UnitTest1
+    public class Day1Tests
     {
         [Test]
         public void TestCalculateExtra()
         {
-            Assert.That(Program.CalculateExtra(14), Is.EqualTo(2));
-            Assert.That(Program.CalculateExtra(1969), Is.EqualTo(966));
-            Assert.That(Program.CalculateExtra(100756), Is.EqualTo(50346));
+            Assert.That(Day1.Program.CalculateExtra(14), Is.EqualTo(2));
+            Assert.That(Day1.Program.CalculateExtra(1969), Is.EqualTo(966));
+            Assert.That(Day1.Program.CalculateExtra(100756), Is.EqualTo(50346));
+        }
+    }
+
+    public class Day2Tests
+    {
+        [Test]
+        public void TestOpcode()
+        {
+            int[] memory = { 1, 0, 0, 0, 99 };
+            Day2.Program.RunOpcodes(memory);
+            int[] expectedOutput = { 2, 0, 0, 0, 99 };
+            Assert.IsTrue(Enumerable.SequenceEqual(memory, expectedOutput));
         }
     }
 }
---
+++ Day2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public class Program
+    {
+        public static bool RunOpcodes(int[] memory)
+        {
+            var ip = 0;
+
+            while (memory[ip] != 99)
+            {
+                switch (memory[ip])
+                {
+                    case 1:
+                        memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]];
+                        break;
+                    case 2:
+                        memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]];
+                        break;
+                    case 99:
+                        break;
+                    default:
+                        return false;
+                }
+                ip += 4;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string file = System.IO.File.ReadAllText(@"day2-part1-input.txt");
+            var codes = file.Split(',');
+            var memory = codes.Select(item => Convert.ToInt32(item)).ToArray();
+
+            int[] workingMemory = new int[memory.Length];
+            Array.Copy(memory, 0, workingMemory, 0, memory.Length);
+
+            workingMemory[1] = 12;
+            workingMemory[2] = 2;
+
+            var result = RunOpcodes(workingMemory);
+
+            Console.WriteLine("Part 1: {0}", workingMemory[0]);
+
+            for(int noun = 0; noun < 100; ++noun)
+            {
+                for(int verb = 0; verb < 100; ++ verb)
+                {
+                    workingMemory = new int[memory.Length];
+                    Array.Copy(memory, 0, workingMemory, 0, memory.Length);
+                    workingMemory[1] = noun;
+                    workingMemory[2] = verb;
+                    if(workingMemory[0] == 19690720)
+                    {
+                        Console.WriteLine("Part 2 - Noun: {0}, Verb: {1}, Result: {2}", noun, verb, 100 * noun + verb);
+                        break;
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
